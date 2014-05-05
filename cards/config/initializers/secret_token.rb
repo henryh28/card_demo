9033f30c502d3c6966288f8164a6493f343f000f
henryh28 <henry@henryhsu.net>
2014-05-05 16:55:50 -0700

    Initial commit

---
+++ cards/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Cards::Application.config.secret_token = 'b1a6dba035b61c638749c1a106ca86f929c61cfeaba2d8cd4f84df149f4bf8fdc59ddbbd50d2633924524f0a1785e3a0b7d2a5c3402688461be286dcd005dc6f'
