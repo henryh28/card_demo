7463e445766c855b525c7f155ebab34759334209
henryh28 <henry@henryhsu.net>
2014-05-09 11:24:05 -0700

    Allows for card edits

--- cards/app/controllers/cards_controller.rb
+++ cards/app/controllers/cards_controller.rb
@@ -10,4 +10,23 @@ class CardsController < ApplicationController
     redirect_to deck_path(@deck)
   end
 
+  def edit
+    @deck = Deck.find(params[:deck_id])
+  end
+
+  def update
+    @deck = Deck.find(params[:deck_id])
+    @card = Card.find(params[:id])
+    if @card.update_attributes(params[:card])
+      redirect_to @deck
+    else
+      render "edit"
+    end
+  end
+
+  def show
+    @card = Card.find(params[:id])
+  end
+
+
 end
