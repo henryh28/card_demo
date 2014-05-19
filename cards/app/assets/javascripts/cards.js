0a8b62ee045120710324c5f9b593930d0d2badd6
henryh28 <henry@henryhsu.net>
2014-05-19 15:19:31 -0700

    separate main game page into multiple partials

--- cards/app/assets/javascripts/cards.js
+++ cards/app/assets/javascripts/cards.js
@@ -1,5 +1,18 @@
-$(function(){
-  $(".card").click(function(){
-    alert ("placeholder")
-  })
-})
+$(document).ready(function(){
+  bindEvent();
+});
+
+
+function bindEvent(){
+  $('.card').click(function(event){
+    event.preventDefault()
+    testFunction(this);
+  });
+};
+
+
+function testFunction(card) {
+  console.log(card)
+  console.log(this.credit > this.cost);
+};
+
--- cards/db/seeds.rb
+++ cards/db/seeds.rb
@@ -7,61 +7,43 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
 starting = Deck.create(name: "starting", deck_type: "player")
-starting.cards.create(card_type: "equipment", effect: "energy", modifier: "1", flavor_text: "starting engine", cost: "0")
-starting.cards.create(card_type: "equipment", effect: "energy", modifier: "1", flavor_text: "starting engine", cost: "0")
-starting.cards.create(card_type: "equipment", effect: "energy", modifier: "1", flavor_text: "starting engine", cost: "0")
-starting.cards.create(card_type: "equipment", effect: "energy", modifier: "1", flavor_text: "starting engine", cost: "0")
-starting.cards.create(card_type: "combat", effect: "attack", modifier: "1", flavor_text: "Lasers!  pew pew!", cost: "0")
-starting.cards.create(card_type: "combat", effect: "attack", modifier: "1", flavor_text: "Lasers!  pew pew!", cost: "0")
-starting.cards.create(card_type: "combat", effect: "attack", modifier: "1", flavor_text: "Lasers!  pew pew!", cost: "0")
-starting.cards.create(card_type: "combat", effect: "attack", modifier: "1", flavor_text: "Lasers!  pew pew!", cost: "0")
-starting.cards.create(card_type: "equipment", effect: "shield", modifier: "1", flavor_text: "Shields up!", cost: "0")
-starting.cards.create(card_type: "equipment", effect: "shield", modifier: "1", flavor_text: "Shields up!", cost: "0")
-starting.cards.create(card_type: "equipment", effect: "shield", modifier: "1", flavor_text: "Shields up!", cost: "0")
-starting.cards.create(card_type: "equipment", effect: "shield", modifier: "1", flavor_text: "Shields up!", cost: "0")
-starting.cards.create(card_type: "resource", effect: "credit", modifier: "1000", flavor_text: "Did someone drop these?", cost: "0")
-starting.cards.create(card_type: "resource", effect: "credit", modifier: "1000", flavor_text: "Did someone drop these?", cost: "0")
-starting.cards.create(card_type: "resource", effect: "credit", modifier: "1000", flavor_text: "Did someone drop these?", cost: "0")
+4.times { starting.cards.create(card_type: "equipment", effect: "energy", modifier: "1", flavor_text: "starting engine", cost: "0") }
+
+4.times { starting.cards.create(card_type: "combat", effect: "attack", modifier: "1", flavor_text: "Lasers!  pew pew!", cost: "0") }
+
+4.times { starting.cards.create(card_type: "equipment", effect: "shield", modifier: "1", flavor_text: "Shields up!", cost: "0") }
+
+3.times { starting.cards.create(card_type: "resource", effect: "credit", modifier: "1000", flavor_text: "Did someone drop these?", cost: "0") }
+
 
 
 main = Deck.create(name: "main", deck_type: "event")
-main.cards.create(card_type: "combat", effect: "enemy", modifier: "1", flavor_text: "a_weak_pirate_01", cost: "0")
-main.cards.create(card_type: "combat", effect: "enemy", modifier: "1", flavor_text: "a_weak_pirate_01", cost: "0")
-main.cards.create(card_type: "combat", effect: "enemy", modifier: "1", flavor_text: "a_weak_pirate_01", cost: "0")
-main.cards.create(card_type: "combat", effect: "enemy", modifier: "1", flavor_text: "a_weak_pirate_01", cost: "0")
-main.cards.create(card_type: "combat", effect: "enemy", modifier: "1", flavor_text: "a_weak_pirate_01", cost: "0")
-main.cards.create(card_type: "combat", effect: "enemy", modifier: "2", flavor_text: "I want yer loot!", cost: "0")
-main.cards.create(card_type: "combat", effect: "enemy", modifier: "2", flavor_text: "I want yer loot!", cost: "0")
-main.cards.create(card_type: "combat", effect: "enemy", modifier: "2", flavor_text: "I want yer loot!", cost: "0")
-main.cards.create(card_type: "combat", effect: "enemy", modifier: "2", flavor_text: "I want yer loot!", cost: "0")
-main.cards.create(card_type: "combat", effect: "enemy", modifier: "3", flavor_text: "Your money and your life", cost: "0")
-main.cards.create(card_type: "combat", effect: "enemy", modifier: "3", flavor_text: "Your money and your life", cost: "0")
-main.cards.create(card_type: "combat", effect: "enemy", modifier: "3", flavor_text: "Your money and your life", cost: "0")
-main.cards.create(card_type: "peace", effect: "none", modifier: "0", flavor_text: "Enjoy it while you can", cost: "0")
-main.cards.create(card_type: "peace", effect: "none", modifier: "0", flavor_text: "Enjoy it while you can", cost: "0")
-main.cards.create(card_type: "peace", effect: "none", modifier: "0", flavor_text: "Enjoy it while you can", cost: "0")
-main.cards.create(card_type: "peace", effect: "none", modifier: "0", flavor_text: "Enjoy it while you can", cost: "0")
-main.cards.create(card_type: "peace", effect: "none", modifier: "0", flavor_text: "Enjoy it while you can", cost: "0")
-main.cards.create(card_type: "bonus", effect: "credit", modifier: "1000", flavor_text: "Guess someone dropped this", cost: "0")
-main.cards.create(card_type: "bonus", effect: "credit", modifier: "1000", flavor_text: "Guess someone dropped this", cost: "0")
-main.cards.create(card_type: "bonus", effect: "credit", modifier: "1000", flavor_text: "Guess someone dropped this", cost: "0")
-main.cards.create(card_type: "penalty", effect: "energy", modifier: "-1", flavor_text: "Regular engine maintenance could have prevented this", cost: "0")
-main.cards.create(card_type: "penalty", effect: "energy", modifier: "-1", flavor_text: "Regular engine maintenance could have prevented this", cost: "0")
-main.cards.create(card_type: "penalty", effect: "credit", modifier: "-1000", flavor_text: "Space IRS.  It's a thing.", cost: "0")
-main.cards.create(card_type: "penalty", effect: "credit", modifier: "-1000", flavor_text: "Space IRS.  It's a thing.", cost: "0")
+5.times { main.cards.create(card_type: "combat", effect: "enemy", modifier: "1", flavor_text: "a_weak_pirate_01", cost: "0") }
+
+4.times { main.cards.create(card_type: "combat", effect: "enemy", modifier: "2", flavor_text: "I want yer loot!", cost: "0") }
+
+3.times { main.cards.create(card_type: "combat", effect: "enemy", modifier: "3", flavor_text: "Your money and your life", cost: "0") }
+
+5.times { main.cards.create(card_type: "peace", effect: "none", modifier: "0", flavor_text: "Enjoy it while you can", cost: "0") }
+
+3.times { main.cards.create(card_type: "bonus", effect: "credit", modifier: "1000", flavor_text: "Guess someone dropped this", cost: "0") }
+
+2.times { main.cards.create(card_type: "penalty", effect: "energy", modifier: "-1", flavor_text: "Regular engine maintenance could have prevented this", cost: "0") }
+
+2.times { main.cards.create(card_type: "penalty", effect: "credit", modifier: "-1000", flavor_text: "Space IRS.  It's a thing.", cost: "0") }
+
 
 buy = Deck.create(name: "buy", deck_type: "upgrades")
-buy.cards.create(card_type: "combat", effect: "attack", modifier: "2", flavor_text: "Green lasers!", cost: "4000")
-buy.cards.create(card_type: "combat", effect: "attack", modifier: "2", flavor_text: "Green lasers!", cost: "4000")
-buy.cards.create(card_type: "combat", effect: "attack", modifier: "2", flavor_text: "Green lasers!", cost: "4000")
-buy.cards.create(card_type: "combat", effect: "attack", modifier: "3", flavor_text: "Purple lasers!", cost: "8000")
-buy.cards.create(card_type: "combat", effect: "attack", modifier: "3", flavor_text: "Purple lasers!", cost: "8000")
-buy.cards.create(card_type: "combat", effect: "defense", modifier: "2", flavor_text: "Rainbow happy shield", cost: "5000")
-buy.cards.create(card_type: "combat", effect: "defense", modifier: "2", flavor_text: "Rainbow happy shield", cost: "5000")
-buy.cards.create(card_type: "resource", effect: "energy", modifier: "2", flavor_text: "I think it's working!", cost: "7000")
-buy.cards.create(card_type: "resource", effect: "energy", modifier: "2", flavor_text: "I think it's working!", cost: "7000")
-buy.cards.create(card_type: "resource", effect: "energy", modifier: "3", flavor_text: "How'd it do that?!", cost: "15000")
-buy.cards.create(card_type: "resource", effect: "energy", modifier: "3", flavor_text: "How'd it do that?!", cost: "15000")
+3.times { buy.cards.create(card_type: "combat", effect: "attack", modifier: "2", flavor_text: "Green lasers!", cost: "4000") }
+
+2.times { buy.cards.create(card_type: "combat", effect: "attack", modifier: "3", flavor_text: "Purple lasers!", cost: "8000") }
+
+2.times { buy.cards.create(card_type: "combat", effect: "defense", modifier: "2", flavor_text: "Rainbow happy shield", cost: "5000") }
+
+2.times { buy.cards.create(card_type: "resource", effect: "energy", modifier: "2", flavor_text: "I think it's working!", cost: "7000") }
+
+2.times { buy.cards.create(card_type: "resource", effect: "energy", modifier: "3", flavor_text: "How'd it do that?!", cost: "15000") }
+
 
 
 
