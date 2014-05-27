a1095cdb6be81b27104f8bf43c42304e413ed038
henryh28 <henry@henryhsu.net>
2014-05-27 15:59:35 -0700

    Player can pay to charge energy at space stations

--- cards/db/migrate/20140523074328_create_ships.rb
+++ cards/db/migrate/20140523074328_create_ships.rb
@@ -1,14 +1,15 @@
 class CreateShips < ActiveRecord::Migration
   def change
     create_table :ships do |t|
-      t.integer :max_energy, :default => "5"
-      t.integer :max_shield, :default => "5"
+      t.integer :max_energy, :default => "10"
+      t.integer :max_shield, :default => "7"
       t.integer :max_hardpoint, :default => "1"
       t.integer :max_speed, :default => "2"
       t.integer :max_fuel, :default => "3"
       t.integer :max_crew, :default => "4"
       t.integer :max_hull, :default => "5"
       t.integer :max_cargo, :default => "10"
+      t.integer :max_attack, :default => "5"
       t.text :cargo_bay
 
       t.timestamps
