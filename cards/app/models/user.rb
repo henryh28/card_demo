e279780df5add9221f8603a09ae3098d676caec5
henryh28 <henry@henryhsu.net>
2014-05-29 18:56:00 -0700

    wip: need to revisit design choices to proceed

--- cards/app/models/user.rb
+++ cards/app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ActiveRecord::Base
-  attr_accessible :username, :password, :email, :energy, :shield, :hardpoint, :speed, :fuel, :crew, :hull, :credit, :attack, :max_cargo, :cargo_bay
+  # attr_accessible :username, :password, :email, :energy, :shield, :hardpoint, :speed, :fuel, :crew, :hull, :credit, :attack, :max_cargo, :cargo_bay
 
   serialize :cargo_bay
 end
